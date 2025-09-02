ca52fba749bb44817f6a8e026cb29c1ba2ee082f
Grey Elerson <grey.elerson@fauna.com>
2025-09-02 12:52:18 -0600

    chore(vercel): remove unused import to fix failing deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import NavbarComponent from './components/Navbar/Navbar'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router>
       <NavbarComponent />
-      <div>
-        
-      </div>
-     <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} /> */}
       </Routes>
     </Router>
   )

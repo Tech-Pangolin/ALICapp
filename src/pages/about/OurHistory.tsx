fa6987fdc58ceaf57c3dbf6df97bba6f52959298
Terra Taylor <terra.taylor@gmail.com>
2025-09-05 07:22:51 -0500

    placeholder history page

---
+++ src/pages/about/OurHistory.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OurHistory: React.FC = () => {
+    return (
+        <div>
+            <h1>Our History</h1>
+            <p>Welcome to the history of our organization. Here, we share our journey and milestones.</p>
+        </div>
+    );
+};
+
+export default OurHistory;

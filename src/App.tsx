d5f77ac8ecd4afaa72a6e62326f2d9aecd2d3aab
Terra Taylor <terra.taylor@gmail.com>
2025-09-05 10:08:31 -0500

    build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Import AOS CSS
 import NavbarComponent from './components/Navbar/Navbar'

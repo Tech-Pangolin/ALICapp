69f66a1d95c0c7f59c6587f4587d87a0e566c35a
Grey Elerson <grey.elerson@fauna.com>
2025-09-05 12:09:54 -0600

    feat(navbar): add "celebrating 120 years" brand section with styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import AboutMenu from './AboutMenu';
 import SolutionsMenu from './SolutionsMenu';
 import ResourcesMenu from './ResourcesMenu';
 import ContactMenu from './ContactMenu';
-import '../base.css'
+import './Navbar.css'
 
 function NavbarComponent() {
   const [activeMegaMenu, setActiveMegaMenu] = useState<string | null>(null);
@@ -50,7 +50,10 @@ function NavbarComponent() {
               height="60"
               className="me-2 my-3"
             />
-         
+            <div id="brand-celebrating">
+              <p id="celebrating">Celebrating</p> 
+              <p id="years">120 Years</p>
+            </div>
           </Navbar.Brand>
 
           {/* Toggle for mobile */}

03fabec7f91d5a4cc12f2ab5de614d14c7a1e4ff
Terra Taylor <terra.taylor@gmail.com>
2025-09-09 12:36:50 -0500

    updates

--- src/pages/about/ourteam/ExecutiveLeadership.tsx
+++ src/pages/about/ourteam/ExecutiveLeadership.tsx
@@ -4,26 +4,26 @@ import PhotoCard from '../../../components/utilities/PhotoCard';
 
 const ExecutiveLeadership: React.FC = () => {
     return (
-        <Container className="about-container">
-            <Row>
-                <Col>
+        <Container className="about-container executive-leadership-container">
+            <Row className='photo-card-row'>
+                <Col xs={12} md={4}>
                     <PhotoCard name="Eric L. Holoman" title="President & Chief Executive Officer" image="https://placehold.co/400x300" />
                 </Col>
-                <Col>
+                <Col xs={12} md={4}>
                     <PhotoCard name="Ryan L. Smith" title="Executive Vice President" image="https://placehold.co/400x300" />
                 </Col>
-                <Col>
+                <Col xs={12} md={4}>
                     <PhotoCard name="Paul A. Miller" title="Corporate Secretary & Treasurer" image="https://placehold.co/400x300" />
                 </Col>
             </Row>
-            <Row>
-                <Col>
+            <Row className='photo-card-row'>
+                <Col xs={12} md={4}>
                     <PhotoCard name="Kam Harris" title="Vice President, Actuary" image="https://placehold.co/400x300" />
                 </Col>
-                <Col>
+                <Col xs={12} md={4}>
                     <PhotoCard name="Courtney Johnson" title="Vice President, Business Development" image="https://placehold.co/400x300" />
                 </Col>
-                <Col>
+                <Col xs={12} md={4}>
                     <PhotoCard name="Trish Payne" title="Vice President, Business Development" image="https://placehold.co/400x300" />
                 </Col>
             </Row>

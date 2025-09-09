9487af1afdd382d0911626805119631e99fcb8e9
Grey Elerson <grey.elerson@fauna.com>
2025-09-09 12:06:59 -0600

    fix(header): make the title an optional param

--- src/components/shared/ContentPageHeader.tsx
+++ src/components/shared/ContentPageHeader.tsx
@@ -12,14 +12,13 @@ const ContentPageHeader: React.FC<{src: string, title: string}> = ({src, title})
             <div className="position-absolute w-100 h-100" />
             
             {/* Text overlay */}
-            <div className="position-absolute w-100 h-100 d-flex align-items-center align-items-md-end justify-content-center justify-content-md-start p-3">
+            {title && <div className="position-absolute w-100 h-100 d-flex align-items-center align-items-md-end justify-content-center justify-content-md-start p-3">
                 <div className="position-absolute w-45 h-25 text-overlay-bg d-flex align-items-center justify-content-center" id="title-text-overlay">
                     <h1 className="title-text">
                         {title}
                     </h1>
                 </div>
-                
-            </div>
+            </div>}
         </div>
     );
 };

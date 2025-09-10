91aa152d16610a481e58734c31ef4d748c37dcb0
Grey Elerson <grey.elerson@fauna.com>
2025-09-09 20:47:01 -0600

    feat(newsroom):  fill newsroom page with placeholder content and components

--- src/pages/about/newsroom/Newsroom.tsx
+++ src/pages/about/newsroom/Newsroom.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ContentPageHeader from '../../../components/shared/ContentPageHeader';
 import ImageCarousel from '../../../components/shared/ImageCarousel';
+import PhotoCard from '../../../components/utilities/PhotoCard';
 
 const Newsroom: React.FC = () => {
     // Team images for development - replace with actual newsroom images when available
@@ -15,10 +16,11 @@ const Newsroom: React.FC = () => {
     return (
         <Container className="about-container">
             <ContentPageHeader src="/img/about/shared/bd-header.png" title="Newsroom" />
-            
+
             <Row className="mt-4">
                 <Col>
-                    <ImageCarousel 
+                    <ImageCarousel
+                        headerText="One Stop for All Things News and Media"
                         images={teamImages}
                         showDots={true}
                         showArrows={true}
@@ -29,7 +31,74 @@ const Newsroom: React.FC = () => {
                     />
                 </Col>
             </Row>
-            
+
+            <h6>View All Our Stories</h6>
+            <Row className='photo-card-row'>
+                <Col xs={12} md={4}>
+                    <PhotoCard
+                        layout="top"
+                        name="Eric L. Holoman"
+                        title="President & Chief Executive Officer"
+                        setSelectedCard={() => { }}
+                        image="https://placehold.co/400x300"
+                        alignment='start'
+                    />
+                </Col>
+                <Col xs={12} md={4}>
+                    <PhotoCard 
+                        layout="top" 
+                        name="Ryan L. Smith" 
+                        title="Executive Vice President" 
+                        setSelectedCard={() => { }} 
+                        image="https://placehold.co/400x300" 
+                        alignment='center' 
+                    />
+                </Col>
+                <Col xs={12} md={4}>
+                    <PhotoCard 
+                        layout="top" 
+                        name="Ryan L. Smith" 
+                        title="Executive Vice President" 
+                        setSelectedCard={() => { }} 
+                        image="https://placehold.co/400x300" 
+                        alignment='center' 
+                    />
+                </Col>
+            </Row>
+
+            <Row className='photo-card-row'>
+                <Col xs={12} md={4}>
+                    <PhotoCard
+                        layout="top"
+                        name="Eric L. Holoman"
+                        title="President & Chief Executive Officer"
+                        setSelectedCard={() => { }}
+                        image="https://placehold.co/400x300"
+                        alignment='start'
+                    />
+                </Col>
+                <Col xs={12} md={4}>
+                    <PhotoCard 
+                        layout="top" 
+                        name="Ryan L. Smith" 
+                        title="Executive Vice President" 
+                        setSelectedCard={() => { }} 
+                        image="https://placehold.co/400x300" 
+                        alignment='center' 
+                    />
+                </Col>
+                <Col xs={12} md={4}>
+                    <PhotoCard 
+                        layout="top" 
+                        name="Ryan L. Smith" 
+                        title="Executive Vice President" 
+                        setSelectedCard={() => { }} 
+                        image="https://placehold.co/400x300" 
+                        alignment='center' 
+                    />
+                </Col>
+            </Row>
+
         </Container>
     );
 };
